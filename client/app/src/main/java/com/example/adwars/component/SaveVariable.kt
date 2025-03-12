041adb3668d1815cd331e67928e181edfebbee5e
XavierCoupe <123065496+XavierCoupe@users.noreply.github.com>
2025-03-12 12:43:56 +0100

    [Autre] Test saveable variable

---
+++ client/app/src/main/java/com/example/adwars/component/SaveVariable.kt
@@ -0,0 +1,42 @@
+package com.example.adwars.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SaveVariable(
+    modifier: Modifier = Modifier
+) {
+
+    var counter by rememberSaveable { mutableIntStateOf(0) }
+
+    Button(
+        onClick = {
+            counter += 1
+        },
+        modifier = modifier
+
+    ) {
+        Text(text = counter.toString())
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun VariablePreview() {
+    Scaffold { innerPadding ->
+       SaveVariable()
+    }
+}
