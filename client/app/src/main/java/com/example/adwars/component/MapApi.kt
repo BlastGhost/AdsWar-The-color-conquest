2de939e4eaa0b361f9d27db57ada4cf3233e4ea3
XavierCoupe <123065496+XavierCoupe@users.noreply.github.com>
2025-03-12 12:43:39 +0100

    [Map] Init map

---
+++ client/app/src/main/java/com/example/adwars/component/MapApi.kt
@@ -0,0 +1,38 @@
+package com.example.adwars.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mapbox.geojson.Point
+import com.mapbox.maps.extension.compose.MapboxMap
+import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+
+@Composable
+fun MapApi(
+    modifier: Modifier = Modifier
+) {
+    MapboxMap(
+        modifier,
+        mapViewportState = rememberMapViewportState {
+            setCameraOptions {
+                zoom(2.0)
+                center(Point.fromLngLat(-98.0, 39.5))
+                pitch(0.0)
+                bearing(0.0)
+            }
+        },
+    )
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun MapPreview() {
+    Box(
+        modifier = Modifier
+    ) {
+        MapApi()
+    }
+}
