fe252c51f5e4cd8f1b9718160967b63b54c52b49
BlastGhost <47760486+BlastGhost@users.noreply.github.com>
2025-03-26 17:04:27 +0100

    [Client] Edit Modifiers style

--- client/app/src/main/java/com/example/adwars/ui/theme/Modifiers.kt
+++ client/app/src/main/java/com/example/adwars/ui/theme/Modifiers.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -14,9 +16,12 @@ import androidx.compose.ui.unit.dp
 
 class Modifiers {
     companion object {
-        val borderRadiusSmall = RoundedCornerShape(6.dp)
-        val borderRadiusMedium = RoundedCornerShape(10.dp)
-        val borderRadiusLarge = RoundedCornerShape(12.dp)
+        val borderRadiusSmallDp = 6.dp
+        val borderRadiusMediumDp = 10.dp
+        val borderRadiusLargeDp = 12.dp
+        val borderRadiusSmall = RoundedCornerShape(borderRadiusSmallDp)
+        val borderRadiusMedium = RoundedCornerShape(borderRadiusMediumDp)
+        val borderRadiusLarge = RoundedCornerShape(borderRadiusLargeDp)
 
         val paddingSmall = 6.dp
         val paddingMedium = 10.dp
@@ -30,6 +35,8 @@ class Modifiers {
         val fontSizeMedium = TextUnit(18f, TextUnitType.Sp)
         val fontSizeLarge = TextUnit(24f, TextUnitType.Sp)
 
+        val fontSizeBigTitle = TextUnit(56f, TextUnitType.Sp)
+
 
         val BOX = Modifier
             .fillMaxWidth()
@@ -38,5 +45,9 @@ class Modifiers {
         val INPUT_FIELD = BOX
             .background(Color.White)
             .border(2.dp, Color.Black, shape = borderRadiusMedium)
+            .height(40.dp)
+            .width(40.dp)
+
+        val SEARCH_BAR = INPUT_FIELD
     }
 }
