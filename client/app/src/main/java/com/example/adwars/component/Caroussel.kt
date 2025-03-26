449748ec963ad6088568d01d7933b704c4afd207
XavierCoupe <123065496+XavierCoupe@users.noreply.github.com>
2025-03-26 10:38:31 +0100

    [Fix] Remove Container

--- client/app/src/main/java/com/example/adwars/component/Caroussel.kt
+++ client/app/src/main/java/com/example/adwars/component/Caroussel.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.adwars.R
 import com.example.adwars.ui.theme.AdWarsTheme
 
 
