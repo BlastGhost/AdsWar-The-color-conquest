f2087e1bf752ffdb9954d10884ca3a49cc273ba4
BlastGhost <47760486+BlastGhost@users.noreply.github.com>
2025-03-26 09:44:25 +0100

    [Client] Fix PlayerSaver

--- client/app/src/main/java/com/example/adwars/system/savers/PlayerSaver.kt
+++ client/app/src/main/java/com/example/adwars/system/savers/PlayerSaver.kt
@@ -7,11 +7,13 @@ object PlayerSaver {
     val saver = mapSaver(
         save = { player ->
             mapOf(
+                "id" to player.id,
                 "username" to player.username
             )
         },
         restore = {
             Player(
+                it["id"] as String,
                 it["username"] as String,
             )
         }
